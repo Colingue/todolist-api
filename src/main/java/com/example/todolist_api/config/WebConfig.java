418a3e433f31c22486e7fd83b31640c648f02646
colingue <colin.noiret@gmail.com>
2024-10-18 15:23:26 +0200

    Update CORS configuration to allow multiple origins and HTTP methods

--- src/main/java/com/example/todolist_api/config/WebConfig.java
+++ src/main/java/com/example/todolist_api/config/WebConfig.java
@@ -9,6 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**");
+        registry.addMapping("/api/**")
+        .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
+
     }
 }
